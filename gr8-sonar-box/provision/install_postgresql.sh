087bc249e3ec6e8ff7e89ba4ad6071459de89da7
Cassio Dias <cassiodias@admins-MacBook-Pro-10.local>
2015-08-25 17:27:53 +0100

    adding sonar subfolder; adding psql version and enabling remote access auto;

--- gr8-sonar-box/provision/install_postgresql.sh
+++ gr8-sonar-box/provision/install_postgresql.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
-sudo apt-get install postgresql postgresql-contrib -y
-# create user and data base
-sudo -u postgres psql -f /vagrant/provision/sonar.sql
+VERSION=9.1
+
+sudo apt-get install postgresql-$VERSION postgresql-contrib -y
+
+# create SONAR user and data base
+sudo -u postgres psql -f /vagrant/provision/sonar/sonar.sql
+
+echo "Configuring postgreSQL $VERSION to listen for remote connections..."
+CONF=/etc/postgresql/$VERSION/main/postgresql.conf
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" $CONF
+
+echo "Configuring postgreSQL $VERSION to accept remote connections (from any host)..."
+cat >> /etc/postgresql/$VERSION/main/pg_hba.conf <<EOF
+# Accept all IPv4 connections - CHANGE THIS!!!
+host    all         all         0.0.0.0/0             md5
+EOF
+
+echo "restarting postgresql..."
+sudo /etc/init.d/postgresql restart
 echo "postgresql is running!"
 
 # Enabling remote connections (http://www.thegeekstuff.com/2014/02/enable-remote-postgresql-connection/):
--- gr8-sonar-box/provision/install_sonar.sh
+++ gr8-sonar-box/provision/install_sonar.sh
@@ -8,13 +8,13 @@ unzip sonarqube-5.1.2.zip
 mkdir /usr/sonarqube
 sudo mv sonarqube-5.1.2 /usr/sonarqube
 
-echo "Installing Gr8 plugin"
+echo "Installing Gr8 plugin..."
 sudo wget https://sonarsource.bintray.com/Distribution/org.sonarsource.sonar-groovy-plugin/sonar-groovy-plugin-1.2.jar -P /usr/sonarqube/sonarqube-5.1.2/extensions/plugins/
 
-echo "Copying sonar.properties with postgresql support"
-sudo cp -rf /vagrant/provision/sonar.properties /usr/sonarqube/sonarqube-5.1.2/conf/sonar.properties
+echo "Copying sonar.properties with postgresql support..."
+sudo cp -rf /vagrant/provision/sonar/sonar.properties /usr/sonarqube/sonarqube-5.1.2/conf/sonar.properties
 
-echo "Bringing up sonarqube"
+echo "Bringing up sonarqube..."
 /usr/sonarqube/sonarqube-5.1.2/bin/linux-x86-64/sonar.sh start
 echo "sonarqube is running!"
 
