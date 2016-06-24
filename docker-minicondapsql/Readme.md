2de6d3d64de550789951421823eb2f13545b49e2
Ian Huston and Matteo Calchetti <rocketfca+ian+matteo@gmail.com>
2016-06-24 18:01:50 +0100

    changing a bit

--- docker-minicondapsql/Readme.md
+++ docker-minicondapsql/Readme.md
@@ -1,15 +1,15 @@
 From
 
-https://hub.docker.com/r/continuumio/miniconda/
+docker pull cassiodias/minicondapsql
 
 build:
 
 $ docker build -t pivotal/minicondapsql .
 
-running with web-app:
-
-$ docker run -i -t -p 8888:8888 pivotal/minicondapsql
-
 running without web-app:
 
 $ docker run -i -t pivotal/minicondapsql /bin/bash
+
+running with web-app:
+
+$ docker run -i -t -p 8888:8888 pivotal/minicondapsql
--- docker-minicondapsql/entry.sh
+++ docker-minicondapsql/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-psql --version
+# psql --version
 
-/opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser
-echo "Miniconda started."
+# /opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser
+# echo "Miniconda started."
